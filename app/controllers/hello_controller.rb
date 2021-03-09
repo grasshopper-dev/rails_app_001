505890692e99eaacb72792a36b5b8037115ac50f
grasshopper-dev <grasshopperdevel2019@gmail.com>
2021-03-09 09:35:29 +0900

    Viewを修正

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,18 +1,6 @@
 class HelloController < ApplicationController
     def index
-        if params['msg'] != nil then
-            msg = 'Hello, '+ params['msg'] + '!'
-        else
-            msg = 'this is sample page.'
-        end
-        msg = '
-        <html>
-        <body>
-           <h1>Sample Page</h1>
-           <p>' + msg + '</p>
-        </body>
-        </html>
-        '
-        render html:msg.html_safe
+        @title = "View Sample"
+        @msg = "コントローラーに用意した値です。"
     end
 end
