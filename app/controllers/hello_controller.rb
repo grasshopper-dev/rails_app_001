17021ea7223cc13fca5a12132761882f76c7ce1d
grasshopper-dev <grasshopperdevel2019@gmail.com>
2021-03-12 14:54:09 +0900

    ヘッダーとフッターを作成

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,20 +1,11 @@
 class HelloController < ApplicationController
+    layout 'hello'
 
     def index
-        if request.post? then
-            @title = 'Result'
-            if params['s1'] then
-                @msg = 'you selected: '
-                for val in params['s1'] do
-                    @msg += val + ' '
-                end
-            else
-                @msg = 'not selected...'
-            end
-        else
-            @title = 'Index'
-            @msg = 'select radio button...'
-        end
+        @header = 'layout sample'
+        @footer = 'copyright Y-T 2021.'
+        @title = 'New Layout'
+        @msg = 'this is sample page!'
     end
 
     def other
